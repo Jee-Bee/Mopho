9bf0e58b15b33b3573704121e67982a1c48a24ec
Jee-Bee <tewinkel.tjm@gmail.com>
2022-04-14 11:41:16 +0200

    Able to open  mainwindow now

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide2.QtWidgets import QApplication
+from src.mainwindow import main
 
 # https://pythonspot.com/pyqt5-grid-layout/
 # https://doc.qt.io/qtforpython/overviews/layout.html
 # See structure https://github.com/tlecomte/friture
 
 if __name__ == "__main__":
-    app = QApplication([])
-    # ...
-    sys.exit(app.exec_())
+    main()
--- src/mainwindow.py
+++ src/mainwindow.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import sys
 
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2 import QtCore
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
@@ -14,16 +15,24 @@ class MainWindow(QMainWindow):
         self.load_ui()
 
     def load_ui(self):
+        # https://stackoverflow.com/questions/14892713/how-do-you-load-ui-files-onto-python-classes-with-pyside/14894550#14894550
+        # https://stackoverflow.com/questions/7144313/loading-qtdesigners-ui-files-in-pyside
         loader = QUiLoader()
-        path = os.fspath(Path(__file__).resolve().parent / "../ui/Mopho.ui")
+        path = os.fspath(Path(__file__).resolve().parents[1] / "ui/Mopho.ui")
+        # path = os.fspath(Path(__file__).resolve().parents[1] / "ui/simpleui.ui")
+        print(path)
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
+        ui_window = loader.load(ui_file, self)
+        ui_window.show()
         ui_file.close()
 
+def main():
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    # widget.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    app = QApplication([])
-    widget = MainWindow()
-    widget.show()
-    sys.exit(app.exec_())
+    mainw = main()
